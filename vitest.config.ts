8faed8e8b6db5262eca9301e565a793412a7e240
Daniel Blignaut <danielblignaut@gmail.com>
2025-12-08 15:19:05 +0000

    update for new version

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,37 @@
-import { mergeConfig } from "vitest/config";
-import baseConfig from "../../../vitest.config.base";
+import { defineConfig } from "vitest/config";
 
-export default mergeConfig(baseConfig, {});
+export default defineConfig({
+	test: {
+		globals: true,
+		environment: 'node',
+		passWithNoTests: true,
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'json', 'html', 'lcov'],
+			reportsDirectory: './coverage',
+			include: [
+				'src/**/*.ts',
+				'src/**/*.tsx',
+				'src/**/*.js',
+				'src/**/*.jsx',
+				'*.ts',
+				'*.tsx',
+				'*.js',
+				'*.jsx',
+			],
+			exclude: [
+				'**/*.test.ts',
+				'**/*.test.tsx',
+				'**/*.spec.ts',
+				'**/*.spec.tsx',
+				'**/*.d.ts',
+				'**/node_modules/**',
+				'**/dist/**',
+				'**/.{git,cache}/**',
+				'**/vitest.config.*',
+				'**/vite.config.*',
+				'**/*.config.*',
+			],
+		},
+	},
+})
